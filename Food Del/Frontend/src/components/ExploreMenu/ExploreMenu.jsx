db6a0c5fe0a384c8e101eb59ed4b8179545ec662
jeylanab <jeylantesi@gmail.com>
2025-01-06 17:45:05 +0530

    added image tag for the food items

--- Food Del/Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Food Del/Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -10,7 +10,7 @@ export const ExploreMenu = ({category, setCategory}) => {
           <div className='explore-menu-list'>
             {menu_list.map((item, index) => {
               return (
-                <div onClick={()=>setCategory(prev=>orev===item.menu_name?'All':item.menu_name)} className='explore-menu-list-item' key={index}>
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?'All':item.menu_name)} className='explore-menu-list-item' key={index}>
                   <img className={category===item.menu_name?"active":""} src={item.menu_image} alt={item.name} />
                   <p>{item.menu_name}</p>
                 </div>
